4cedb6386f6da61d78eb7db10b44ded665709eca
gabviana <gabriela_viana13@hotmail.com>
2020-11-21 12:24:44 +0100

    maquetacion frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import './App.css';
+import React, { Fragment } from 'react';
+
 const  App = () => {
   return (
-    <div>
-			<h1 style={{textAlign: "center"}}>
-				Nuclio FullStack Development Hackaton 2
+    <Fragment>
+    <div className="contenedor">
+			<h1>
+				Nuclio FullStack Development Hackaton 2: Covid Hackaton
 			</h1>
     </div>
+    <div className="casosactivos">
+      <div className="world">
+        <p>World Total cases</p>
+      </div>
+      <div className="totalspain">
+        <h1>SPAIN</h1>
+      </div>
+    </div>
+      <div className="spaindeads">
+        <h1>SPAINDEADS</h1>
+      </div>
+      <div className="pobreza">
+        <h1>POBREZA</h1>
+      </div>
+    </Fragment>
   );
 }
 
